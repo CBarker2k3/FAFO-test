2f04e533a84cab299a0d4210cbecc0438c7d375a
Primus2k3 <cameron.barker@3gi.co.uk>
2023-10-13 17:12:10 +0100

    upload flask

---
+++ FAFO-test/index.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+sys.path.append('/')
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+@route('/', methods=['GET'])
+def index(self):
+    return render_template('HTMLTEST.html')
+
+Flasky.register(app,route_base = '/')
+app.logger.removeHandler(default_handler)
+
+
+if __name__ == "__main__":
+  app.run(debug='True', host='0.0.0.0', port=3000)
